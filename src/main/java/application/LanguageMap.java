feb4a8e48175eed4e2e45ce291e9f05de2b561db
ppcat <wuyifan96246@163.com>
2022-12-12 17:51:56 +0800

    添加了json解析，更新了角色台词，添加角色语音，更新了maven依赖

---
+++ src/main/java/application/LanguageMap.java
@@ -0,0 +1,32 @@
+package application;
+
+public class LanguageMap {
+    private String simplifiedChinese;
+    private String Japanese;
+    private String English;
+    private String Korean;
+    private String traditionalChinese;
+
+    public LanguageMap(String simplifiedChinese, String japanese, String english, String korean, String traditionalChinese) {
+        this.simplifiedChinese = simplifiedChinese;
+        Japanese = japanese;
+        English = english;
+        Korean = korean;
+        this.traditionalChinese = traditionalChinese;
+    }
+    public String getDialog(String language) {
+        switch (language) {
+            case "simplifiedChinese":
+                return simplifiedChinese;
+            case "Japanese":
+                return Japanese;
+            case "English":
+                return English;
+            case "Korean":
+                return Korean;
+            case "traditionalChinese":
+                return traditionalChinese;
+        }
+        return simplifiedChinese;
+    }
+}
