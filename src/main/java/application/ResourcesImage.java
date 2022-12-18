4394d973736283024e1f6c3b1111873885a8fce4
ppcat <wuyifan96246@163.com>
2022-12-18 13:00:56 +0800

    优化了堆溢出的相关处理

--- src/main/java/application/ResourcesImage.java
+++ src/main/java/application/ResourcesImage.java
@@ -1,7 +1,5 @@
 package application;
 
-import com.sun.javafx.iio.gif.GIFDescriptor;
-import com.sun.javafx.iio.gif.GIFImageLoaderFactory;
 import javafx.scene.image.Image;
 
 public class ResourcesImage {
@@ -10,19 +8,20 @@ public class ResourcesImage {
     private Image Move;
     private Image Sleep;
     private Image Interact;
-    private Image Special;
+    private Image Special;  // 好感度解锁后，在新皮肤里可以出现此动作
     private Image MoveF;
 
     public Image getSpecial() {
+        if (Special == null)
+            Special = new Image(ResourcesImage.class.getResource("/" + Main.getPetName() + "/" + Main.getPetSkin() + "/Special.gif").toExternalForm());
         return Special;
     }
 
     public Image getRelax() {
-        if (Relax == null){
-            System.out.println(Main.getPetName()+" \n"+Main.getPetSkin());
+        if (Relax == null) {
+            System.out.println(Main.getPetName() + " \n" + Main.getPetSkin());
             Relax = new Image(ResourcesImage.class.getResource("/" + Main.getPetName() + "/" + Main.getPetSkin() + "/Relax.gif").toExternalForm());
         }
-
         return Relax;
     }
 
@@ -55,63 +54,77 @@ public class ResourcesImage {
             MoveF = new Image(ResourcesImage.class.getResource("/" + Main.getPetName() + "/" + Main.getPetSkin() + "/MoveF.gif").toExternalForm());
         return MoveF;
     }
-    public void switchSkin(){
-        Relax=null;
-        Sit=null;
-        Move=null;
-        MoveF=null;
+
+    public void switchSkin() {
+        Relax = null;
+        Sit = null;
+        Move = null;
+        MoveF = null;
         Sleep = null;
-        Interact=null;
-        if(Main.getPetSkin().equals("default")){
+        Interact = null;
+        Special = null;
+        if (Main.getPetSkin().equals("default")) {
             Main.setPetSkin("plant");
-        }
-        else if(Main.getPetSkin().equals("plant")){
+        } else if (Main.getPetSkin().equals("plant")) {
             Main.setPetSkin("winter");
-        }
-        else{
+        } else {
             Main.setPetSkin("default");
         }
         System.out.println(Main.getPetSkin());
         Main.getImageView().setImage(Main.getUi().getImage("Relax"));
     }
+
     /*
-        *注意由于美术资源还未加入，切换宠物功能暂时不能使用，因为懒得给biu和lxh的gif改名字
+     *注意由于美术资源还未加入，切换宠物功能暂时不能使用，因为懒得给biu和lxh的gif改名字
      */
-    public void switchPet(){
-        Relax=null;
-        Sit=null;
-        Move=null;
-        MoveF=null;
+    public void switchPet() {
+        Relax = null;
+        Sit = null;
+        Move = null;
+        MoveF = null;
         Sleep = null;
-        Interact=null;
-        if(Main.getPetName().equals("002_amiya")){
+        Interact = null;
+        Special = null;
+        if (Main.getPetName().equals("002_amiya")) {
             //Main.setPetSkin(""); 等待加入美术资源
-        }
-        else if(Main.getPetName().equals("plant")){
+        } else if (Main.getPetName().equals("plant")) {
             //Main.setPetSkin(""); 等待加入美术资源
-        }
-        else{
-          //  Main.setPetSkin("");  等待加入美术资源
+        } else {
+            //  Main.setPetSkin("");  等待加入美术资源
         }
         System.out.println(Main.getPetName());
         Main.getImageView().setImage(Main.getUi().getImage("Relax"));
     }
+
+    /**
+     * catch堆溢出错误，将除Relax和Interact以外的图片资源全部释放掉
+     * @param behavior
+     * @return
+     */
     public Image getImage(String behavior) {
-        switch (behavior) {
-            case "Relax":
-                return getRelax();
-            case "Interact":
-                return getInteract();
-            case "Move":
-                return getMove();
-            case "Sit":
-                return getSit();
-            case "Sleep":
-                return getSleep();
-            case "Special":
-                return getSpecial();
-            case "MoveF":
-                return getMoveF();
+        try {
+            switch (behavior) {
+                case "Relax":
+                    return getRelax();
+                case "Interact":
+                    return getInteract();
+                case "Move":
+                    return getMove();
+                case "Sit":
+                    return getSit();
+                case "Sleep":
+                    return getSleep();
+                case "Special":
+                    return getSpecial();
+                case "MoveF":
+                    return getMoveF();
+            }
+        } catch (OutOfMemoryError e) {
+            Sit = null;
+            Move = null;
+            MoveF = null;
+            Sleep = null;
+            return getImage(behavior);
         }
         return Relax;
     }
